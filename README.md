55ff87c0d8213b5c264718420acd4897ed248c37
amisha26 <amisha8299@gmail.com>
2024-01-03 14:35:46 -0500

    Doc - added Readme file

---
+++ README.md
@@ -0,0 +1,9 @@
+## Admin Dashboard
+
+### Description (Ongoing)
+
+This is a MERN stack Admin Dashboard.
+
+Frontend -  Material UI, Material UI Data Grid, Nivo Charts, Redux Toolkit and Redux Toolkit Query.
+Backend - Node JS, Express Js, Mongoose, and MongoDB. 
+Data Model using Entity Relationship Diagrams as well as making aggregate calls in MongoDB.
--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
+import { useMemo } from "react";
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="App">
     </div>
