08f83fa9b359b39d7daa633631896bdd3ea9d1e0
amisha26 <amisha8299@gmail.com>
2024-01-04 22:56:49 -0500

    feat(client) - sidebar imports added

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import {
+    Box,
+    Divider,
+    Drawer,
+    IconButton,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Typography,
+    useTheme
+} from "@mui/material";
+import {
+    SettingsOutlines,
+    ChevronLeft,
+    ChevronRightOutlined,
+    HomeOutlined,
+    ShoppingCartOutlined,
+    Groups2Outlined,
+    ReceiptLongOutlined,
+    PublicOutlined,
+    PointOfSaleOutlined,
+    TodayOutlined,
+    CalendarMonthOutlined,
+    AdminPanelSettingsOutlined,
+    TrendingUpOutlined,
+    PieChartOutlined
+} from "@mui/icons-material";
+
+
+const Sidebar = () => {
+  return 
+}
+
+export default Sidebar
